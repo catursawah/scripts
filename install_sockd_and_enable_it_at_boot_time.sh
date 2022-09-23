bd4b7f20449647945b349ba64485648b41cdad6b
catursawah <83405015+catursawah@users.noreply.github.com>
2022-09-23 11:34:12 +0700

    Update install_sockd_and_enable_it_at_boot_time.sh

--- install_sockd_and_enable_it_at_boot_time.sh
+++ install_sockd_and_enable_it_at_boot_time.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
 apt update;apt -y install gcc cmake binutils build-essential net-tools
-ifaceName=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}')
 wget https://www.inet.no/dante/files/dante-1.4.3.tar.gz
 tar -xf dante-1.4.3.tar.gz
 cd dante-1.4.3
@@ -10,20 +8,15 @@ make install
 
 cat > /etc/sockd.conf <<EOF
 logoutput: /var/log/socks.log
-
-internal: $ifaceName port = 1080
-external: $ifaceName
-
+internal: eth0 port = 1080
+external: eth0
 socksmethod: username
-
 user.unprivileged: nobody
 user.privileged: root
-
 client pass {
 from: 0.0.0.0/0 to: 0.0.0.0/0
 log: error
 }
-
 socks pass {
 from: 0.0.0.0/0 to: 0.0.0.0/0
 command: connect
@@ -35,8 +28,8 @@ EOF
 
 /usr/local/sbin/sockd -D &
 
-useradd --shell /usr/sbin/nologin mikrotik999
-echo "mikrotik999:Elibawnos" | chpasswd
+useradd --shell /usr/sbin/nologin oiziq
+echo "oiziq:Bandung12" | chpasswd
 
 netstat -ntlp
 
@@ -45,7 +38,6 @@ cat > /etc/systemd/system/startsockd.service <<EOL
 Description=start sockd at boot time
 After=network.target
 StartLimitIntervalSec=0
-
 [Service]
 Type=simple
 Restart=always
@@ -56,7 +48,6 @@ ExecStart=/usr/local/sbin/sockd -D &
 ExecStartPost
 ExecStop=
 ExecReload=
-
 [Install]
 WantedBy=multi-user.target
 EOL
@@ -65,5 +56,3 @@ systemctl daemon-reload
 systemctl enable startsockd.service
 systemctl start startsockd.service
 systemctl status startsockd.service
-
-
